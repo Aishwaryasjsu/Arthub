96cff6f070b93f7b5b4cad92ee65caddf9742b95
Aishwarya Lodhi <Aishwarya.lodhi@sjsu.edu>
2023-07-24 20:53:17 -0700

    Push changes for login signup

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const pool = require('./config/db');
 const cors = require('cors'); // Import the cors middleware
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 
 // Enable CORS for all routes
 app.use(cors());
@@ -25,17 +25,17 @@ app.post('/signup', (req, res) => {
     return res.status(400).json({ error: 'Username, password, and email are required' });
   }
 
-  // Hash the password before storing it in the database
-  bcrypt.hash(password, 10, (err, hashedPassword) => {
-    if (err) {
-      console.error('Error hashing password:', err);
-      return res.status(500).json({ error: 'Something went wrong' });
-    }
+  // // Hash the password before storing it in the database
+  // bcrypt.hash(password, 10, (err, hashedPassword) => {
+  //   if (err) {
+  //     console.error('Error hashing password:', err);
+  //     return res.status(500).json({ error: 'Something went wrong' });
+  //   }
 
   // Insert the data into the database
   pool.query(
     'INSERT INTO Users (username, password, email, isSeller) VALUES (?, ?, ?, ?)',
-    [username, hashedPassword, email, isSeller],
+    [username, password, email, isSeller],
     (err, results) => {
       if (err) {
         console.error('Error inserting data:', err);
@@ -68,28 +68,11 @@ app.post('/login', (req, res) => {
    console.log(results[0]);
     const user = results[0];
 
-    // Compare the provided password with the hashed password stored in the database
-    bcrypt.compare(password, user.password, (bcryptErr, isMatch) => {
-      if (bcryptErr) {
-        console.error('Error comparing passwords:', bcryptErr);
-        return res.status(500).json({ error: 'Something went wrong' });
-      }
-
-      if (!isMatch) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-
-      // Password is correct, user is authenticated
-      // You can now generate a token for token-based authentication (optional)
-      // const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '1h' });
-
-      // Send the token as part of the response (optional)
+   
       return res.status(200).json({ message: 'Login successful' });
     });
   });
-});
 
-});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
